3e0e2b8a397bdf6818879dd67a4bb498fc4dabc5
Felipe Lopes <felipefllp@msn.com>
2021-07-27 22:46:32 -0300

    feat: componentização

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import Header from './components/Header'
+import Main from './components/Main';
+
 export default function App() {
   console.log('Teste no console do navegador');
 
   return (
     <>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Projeto base para o Módulo React I
-          </h1>
-        </div>
-      </header>
-
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
+      <Header>Atividade Prévia</Header>
+      <Main></Main>
     </>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main className="container mx-auto p-4">
+      <h2>O conteúdo fica aqui.</h2>
+    </main>
+  )
+}
